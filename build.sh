a7b08cb3acb331a3330bac16c7068ec832943fc9
Moritz Lell <mlell08@gmail.com>
2021-10-21 12:48:16 +0200

    Check for clean workdir earlier in build process

--- build.sh
+++ build.sh
@@ -3,5 +3,14 @@
 # Build the Singularity container
 set -ue
 
+if [ -z "${DEBUG-}" ]; then
+  # make sure it doesn't fail
+  git status -s &>/dev/null
+  if [ ! "$(git status -s | wc -l)" = "0" ]; then
+    echo "Working dir dirty!" >&2
+    exit 1
+  fi 
+fi
+
 singularity build -F r-py.sif Singularity-r
 singularity build -F rstudio.sif Singularity-rstudio
