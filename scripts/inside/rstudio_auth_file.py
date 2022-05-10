66cf40dd834bbe8b0c67beae1b4b5bea378ffff3
Moritz Lell <mlell08@gmail.com>
2022-05-10 14:54:26 +0200

    Deny access for wrong user name
    When the user name is different that that who started the container, deny.
    Else, we get problems later on, maybe the wrong username is somehowo forwarded.
    The error was: "Could not connect to service" and is shown in the half-loaded
    RStudio window then.
    
    Fixes #10.

--- scripts/inside/rstudio_auth_file.py
+++ scripts/inside/rstudio_auth_file.py
@@ -3,13 +3,15 @@ import os
 import sys
 from crypt import crypt
 import hmac
-from getpass import getpass
+from getpass import getpass,getuser
 
 def msg(str): print(msg, file = sys.stderr)
 
 if len(sys.argv) < 1:
   msg("Usage: auth USERNAME")
   sys.exit(2)
+
+username = sys.argv[1]
 
 pwfile = os.environ["RSTUDIO_PASSWORD_FILE"]
 if pwfile == None:
@@ -21,9 +23,11 @@ with open(pwfile) as f:
 
 testpw = sys.stdin.readline().strip("\n")
 cpw = crypt(testpw, pw)
-correct = hmac.compare_digest(cpw, pw)
 
-if correct:
+correct_user = username == getuser()
+correct_pass = hmac.compare_digest(cpw, pw)
+
+if correct_user and correct_pass:
   sys.exit(0)
 else:
   sys.exit(1)
