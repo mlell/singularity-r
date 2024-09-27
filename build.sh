0c34c5aabf6cdba59385d09dd89afdaafff393bf
Moritz Lell <mlell08@gmail.com>
2024-09-27 12:44:44 +0200

    Re-introduce snapshots.debian.org by using local keyring

--- build.sh
+++ build.sh
@@ -20,10 +20,19 @@ sgb(){
     -B "$PWD/vla:/var/lib/apt" \
     "$1" "$2" # Recipe and output SIF file
 }
-if [ ! -f debian-baseimage.sif ]; then 
-  singularity build -F debian-baseimage.sif Singularity-baseimage
-fi
+#if [ ! -f debian-baseimage.sif ]; then 
+  # Cannot use sgb() here because we start from scratch, so the mounted directores
+  # are not yet there.
+  # Do not auto-cleanup, this would remove files in bind-mounted directories
+  # The paths must be unmounted first. We have to include it here and not
+  # in the sgb() function because here, we bind-mount in the %setup section of
+  # the Singularity recipe. Therefore we have to also take care to unmount it
+  # by ourselves. In sgb(), Singularity makes sure that bind-mounted directories
+  # are not deleted in case of error.
+ # singularity build -F --no-cleanup debian-baseimage.sif Singularity-baseimage
+#fi
 
+sgb debian-baseimage.sif Singularity-baseimage
 sgb r-py.sif Singularity-r
 sgb rstudio.sif Singularity-rstudio
 
